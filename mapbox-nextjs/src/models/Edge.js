37a84a3f52c8f38ad1daffef2860a5a8506aa07c
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-06-13 09:08:27 -0400

    feat: :sparkles: graphic linestring

--- mapbox-nextjs/src/models/Edge.js
+++
@@ -1,6 +0,0 @@
-return class Edge {
-	constructor(source, destination) {
-		this.source = source;
-		this.destination = destination;
-	}
-}
--- mapbox-nextjs/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MapGraphic from '@/components/Map';
-import '@/styles/Home.module.css';
-
-export default function Home() {
-	return <MapGraphic />;
-}
